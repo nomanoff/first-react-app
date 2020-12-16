f46381477bce7680116979312080aad5152f0eb4
aliabdulazeez <aliabdulazeez01@gmail.com>
2020-12-17 06:56:30 +0900

    footer added

--- src/index.js
+++ src/index.js
@@ -1,8 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+let currentYear = new Date();
+
 ReactDOM.render(
-  <h1>Hello World</h1>,
+  <div id="skills">
+
+    <h1>Hello I am Adam</h1>
+    
+    <ul>
+                              
+      <li>HTML</li>
+      <li>CSS</li>
+      <li>JavaScript</li> 
+                          
+    </ul>
+
+    <ul>
+        <li>jQuery</li> 
+        <li>Bootstrap 5</li> 
+        <li>Sass</li> 
+   </ul>  
+
+   <footer>
+     <p>Made by Ali Adam</p>
+     <p>Copyright {currentYear.getFullYear()}</p>
+   </footer>                   
+ </div>,
   document.getElementById('root')
 );
 
